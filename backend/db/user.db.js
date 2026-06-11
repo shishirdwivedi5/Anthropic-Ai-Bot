7f91c9f568e6625fccf6d63b9ee17faefd2d9ccf
shishir dwivedi <shishirdwivedi564@gmail.com>
2026-06-11 12:13:41 +0530

    env change

--- backend/db/user.db.js
+++ backend/db/user.db.js
@@ -2,21 +2,11 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   await mongoose
-    .connect(
-      "mongodb+srv://shishirdwivedi564_db_user:Xpo6CJVDzcj46Qk7@cohort.muam6hq.mongodb.net/AI-DB",
-    )
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log("DB connected "))
     .catch((err) => console.log("DB connection error", err));
 }
 
-// async function connectDB() {
-//   try {
-//     await client.connect();
-//     console.log("db connect");
-//     return client.db("AI");
-//   } catch (error) {
-//     console.log("errorDB", error);
-//   }
-// }
+
 
 module.exports = connectDB;
